25ec1f7575f7ae9fad8ad18528eafa592b4dcd62
António <apgoncalves@outlook.pt>
2020-11-05 14:25:19 +0000

    Improve syntax
    Remove unnecessary multi-line strings and conform syntax to PEP8 (except long lines)

--- probely.py
+++ probely.py
@@ -8,28 +8,27 @@ cm5hbWUiOiJZVWt3WjhHZFhpUmkiLCJqdGkiOiJRRDdoWUFvdjdTYnIifQ.O53R154sjy\
 E0I5iv_ykFkboz7i5qeQwRRk-Kve9hjIs"""}
 
 page_total = requests.get(url, headers=headers).json()["page_total"]
-data = requests.get(url, headers=headers, params={"length": page_total * 10}).json()["results"] # page_total * 10 (default length) = all results in one page
+data = requests.get(url, headers=headers, params={"length": page_total * 10}).json()["results"]  # page_total * 10 (default length) = all results in one request
 
 score = 0
 for severity in [result["severity"] for result in data if result["state"] == "notfixed"]:
-    if severity == 10: # Low Severity
+    if severity == 10:  # Low Severity
         score += 1
-    elif severity == 20: # Medium Severity
+    elif severity == 20:  # Medium Severity
         score += 10
-    elif severity == 30: # High Severity
+    elif severity == 30:  # High Severity
         score += 40
 print(f"Generic Risk Score: {score}\n")
 
 # ----- EXERCISE 2 -----
 print("----- FIXED FINDINGS -----")
 for x in [finding for finding in data if "3hbQvcGEmLbW" in finding["scans"] and "2RnxpEEm2qd5" not in finding["scans"]]:
-    print(f"""ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n""")
+    print(f"ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n")
 
 print("----- UNFIXED FINDINGS -----")
 for x in [finding for finding in data if "3hbQvcGEmLbW" in finding["scans"] and "2RnxpEEm2qd5" in finding["scans"]]:
-    print(f"""ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n""")
-    
+    print(f"ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n")
+
 print("----- NEW FINDINGS -----")
 for x in [finding for finding in data if "3hbQvcGEmLbW" not in finding["scans"] and "2RnxpEEm2qd5" in finding["scans"]]:
-    print(f"""ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n""")
-    
+    print(f"ID: {x['id']}\nURL: {x['url']}\nTYPE: {x['definition']['name']}\n")
